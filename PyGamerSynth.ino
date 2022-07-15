8026148c6c7485ff2e38eed7a2e76708f1a46b4f
Bob Gonsalves <pinknoiz@me.com>
2022-07-14 23:06:08 -0400

    add filter normally on, Button A to set oscillators
    separate display loop from input

--- PyGamerSynth.ino
+++ PyGamerSynth.ino
@@ -1,6 +1,5 @@
 #include <Adafruit_Arcada.h>
 #include <Adafruit_SPIFlash.h>
-#include "audio.h"
 #include <Audio.h>
 #include "Adafruit_seesaw.h"
 #include <TaskScheduler.h>
@@ -23,54 +22,51 @@
 #define _PX(a)
 #endif
 
-AudioSynthWaveform       waveform2;      //xy=165,222
-AudioSynthSimpleDrum     drum1;          //xy=235,398
-AudioSynthWaveform       waveform1;      //xy=266,71
-AudioSynthNoisePink      pink1;          //xy=290,447
-AudioSynthWaveform       waveform3;      //xy=300,283
-AudioSynthWaveformSineHires sine_hires1;    //xy=328,536
-AudioSynthWaveformDc     dc1;            //xy=362,630
-AudioSynthWaveformSineModulated sine_fm1;       //xy=472,474
-AudioEffectMultiply      multiply1;      //xy=483,162
-AudioSynthKarplusStrong  string1;        //xy=519,410
-AudioSynthToneSweep      tonesweep1;     //xy=570,597
-AudioSynthWaveformPWM    pwm1;           //xy=697,570
-AudioSynthWaveformSine   sine1;          //xy=785,363
-AudioOutputAnalogStereo  dac1;           //xy=818,178
-AudioConnection          patchCord1(waveform2, 0, multiply1, 1);
-AudioConnection          patchCord2(waveform1, 0, multiply1, 0);
-AudioConnection          patchCord3(multiply1, 0, dac1, 0);
-AudioConnection          patchCord4(multiply1, 0, dac1, 1);
+// GUItool: begin automatically generated code
+AudioSynthSimpleDrum     drum1;          //xy=111.42855834960938,587.0000591278076
+AudioSynthWaveform       waveform1;      //xy=156.7142791748047,101.4285774230957
+AudioSynthWaveform       waveform2;      //xy=157.14286041259766,179.57142448425293
+AudioSynthNoisePink      pink1;          //xy=166.42855834960938,636.0000591278076
+AudioSynthWaveform       waveform3;      //xy=176.42855834960938,472.0000591278076
+AudioSynthWaveformSineHires sine_hires1;    //xy=204.42855834960938,725.0000591278076
+AudioSynthWaveformDc     dc1;            //xy=238.42855834960938,819.0000591278076
+AudioSynthWaveformSineModulated sine_fm1;       //xy=348.4285583496094,663.0000591278076
+AudioSynthKarplusStrong  string1;        //xy=395.4285583496094,599.0000591278076
+AudioEffectMultiply      multiply1;      //xy=442.2857131958008,143.85715007781982
+AudioSynthToneSweep      tonesweep1;     //xy=446.4285583496094,786.0000591278076
+AudioSynthWaveformSine   sine1;          //xy=449.9999809265137,296.2857208251953
+AudioSynthWaveformPWM    pwm1;           //xy=573.4285583496094,759.0000591278076
+AudioFilterStateVariable filter1;        //xy=624.2857142857142,97.14285714285714
+AudioMixer4              mixer1;         //xy=805.7142857142857,112.85714285714285
+AudioOutputAnalogStereo  dac1;           //xy=1097.2856063842773,92.71429824829102
+AudioConnection          patchCord1(waveform1, 0, multiply1, 0);
+AudioConnection          patchCord2(waveform2, 0, multiply1, 1);
+AudioConnection          patchCord3(multiply1, 0, filter1, 0);
+AudioConnection          patchCord4(sine1, 0, filter1, 1);
+AudioConnection          patchCord5(filter1, 1, mixer1, 0);
+AudioConnection          patchCord6(mixer1, 0, dac1, 1);
+AudioConnection          patchCord7(mixer1, 0, dac1, 0);
 // GUItool: end automatically generated code
-
 
 Adafruit_Arcada arcada;
 extern Adafruit_SPIFlash Arcada_QSPI_Flash;
 
 uint32_t buttons, last_buttons;
+
+int joystickX, joystickY;
+int xoffset, yoffset; // for analog joystick, static offset
+
 uint8_t j = 0;  // neopixel counter for rainbow
 
-int xoffset, yoffset; // for analog joystick
-
 Scheduler taskScheduler;
-#define PERIOD1 50
 void readControls();
-Task tBlink1 (PERIOD1, TASK_FOREVER, &readControls, &taskScheduler, true);
+void updateDisplay();
 
-//// Check the timer callback, this function is called every millisecond!
-//volatile uint16_t milliseconds = 0;
-//void timercallback() {
-//  analogWrite(LED_BUILTIN, milliseconds);  // pulse the LED
-//  if (milliseconds == 0) {
-//    milliseconds = 255;
-//  } else {
-//    milliseconds--;
-//  }
-//}
+Task inputLoop (50, TASK_FOREVER, &readControls, &taskScheduler, true);
+Task displayLoop (5, TASK_FOREVER, &updateDisplay, &taskScheduler, true);
 
 void setup() {
   AudioMemory(120);
-  Serial.println("Hello! Arcada PyGamer test");
   if (!arcada.arcadaBegin()) {
     Serial.print("Failed to begin");
     while (1);
@@ -82,87 +78,125 @@ void setup() {
   arcada.display->setTextWrap(true);
 
   /********** Check QSPI manually */
-  if (!Arcada_QSPI_Flash.begin()){
+  if (!Arcada_QSPI_Flash.begin()) {
     Serial.println("Could not find flash on QSPI bus!");
     arcada.display->setTextColor(ARCADA_RED);
     arcada.display->println("QSPI Flash FAIL");
   }
+
   arcada.accel->setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
   arcada.accel->setClick(1, 80);
 
   xoffset = arcada.readJoystickX();// record static err
   yoffset = arcada.readJoystickY();
-  
+
+  int illum = arcada.readLightSensor();
+  _PP("light "); _PL(illum);
+
   buttons = last_buttons = 0;
 
-//  arcada.timerCallback(1000, timercallback);
+  filter1.frequency(880);
+  filter1.octaveControl(1);
+  filter1.resonance(1);
 
-  waveform1.frequency(440);
-  waveform1.amplitude(.50);
-  waveform2.frequency(441);
-  waveform2.amplitude(.50);
+  waveform1.begin(.50, 440, WAVEFORM_SAWTOOTH);
+  waveform2.begin(.50, 441, WAVEFORM_TRIANGLE);
+  mixer1.gain(0, .5);
 
-  
+  arcada.display->fillScreen(ARCADA_BLACK);
   arcada.display->setTextColor(ARCADA_BLUE);
   arcada.display->println("Hey Doc!");
   arcada.display->println(" wassup");
+  delay(1000);
+  arcada.display->fillRect(0, 70, 160, 60, ARCADA_BLACK);
 }
 
 void loop() {
   taskScheduler.execute();
 
-  for(int32_t i=0; i< arcada.pixels.numPixels(); i++) {
-     arcada.pixels.setPixelColor(i, Wheel(((i * 256 / arcada.pixels.numPixels()) + j*5) & 255));
+  for (int32_t i = 0; i < arcada.pixels.numPixels(); i++) {
+    arcada.pixels.setPixelColor(i, Wheel(((i * 256 / arcada.pixels.numPixels()) + j * 5) & 255));
   }
   arcada.pixels.show();
 
 }
 
 void readControls() {
-  int x = arcada.readJoystickX() - xoffset;
-  int y = arcada.readJoystickY() - yoffset;
-  _PP("X: ");_PP(x);_PP(",  Y: "); _PL(y);
-
-  float pitch1 = map(x, -512, 511, 120, 240);
-  float pitch2 = map(y, -512, 511, .5, 5);
- 
-  waveform1.frequency(pitch1);
-  waveform2.frequency(pitch2);
-
-  uint8_t pressed_buttons = arcada.readButtons(); 
+  uint8_t pressed_buttons = arcada.readButtons();
   last_buttons = buttons;
   buttons = pressed_buttons;
   
-//  distanceSensor.startRanging(); //Write configuration bytes to initiate measurement
-//  int distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
-//  distanceSensor.stopRanging();
- 
- uint8_t click = arcada.accel->getClick();
-  if (click & 0x30) {
-    if (click & 0x10) arcada.display->print("single");
-    if (click & 0x20) arcada.display->print("double");
-    arcada.display->print(" click detected (0x"); arcada.display->print(click, HEX); arcada.display->println("): ");
-   }
+  joystickX = arcada.readJoystickX() - xoffset;
+  joystickY = arcada.readJoystickY() - yoffset;
+  if (buttons & ARCADA_BUTTONMASK_A) {
+    float pitch1 = map(joystickX, -512, 511, 120, 240);
+    float pitch2 = map(joystickY, -512, 511, .80, 5);
+    waveform1.frequency(pitch1);
+    waveform2.frequency(pitch2);
+  }
+  else {
+    filter1.frequency(map(joystickX, -512, 511, 120, 2000));
+  }
 
-  sensors_event_t event; 
+  /*
+
+    #define ARCADA_BUTTONMASK_A 0x01
+    #define ARCADA_BUTTONMASK_B 0x02
+    #define ARCADA_BUTTONMASK_SELECT 0x04
+    #define ARCADA_BUTTONMASK_START 0x08
+  */
+
+  //  distanceSensor.startRanging(); //Write configuration bytes to initiate measurement
+  //  int distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
+  //  distanceSensor.stopRanging();
+
+  uint8_t click = arcada.accel->getClick();
+  if (click & 0x30) {
+    //    if (click & 0x10) arcada.display->print("single");
+    //    if (click & 0x20) arcada.display->print("double");
+    //    arcada.display->print(" click detected (0x"); arcada.display->print(click, HEX); arcada.display->println("): ");
+  }
+
+  sensors_event_t event;
   arcada.accel->getEvent(&event);
 
-//  arcada.display->print("Z:"); arcada.display->print(event.acceleration.z, 1);
-//  Serial.print("Light: "); Serial.println(arcada.readLightSensor());
-//  float vbat = arcada.readBatterySensor();
-   
+  j++; // update neopixels
+
+
+  //  arcada.display->print("Z:"); arcada.display->print(event.acceleration.z, 1);
+  //  Serial.print("Light: "); Serial.println(arcada.readLightSensor());
+  //  float vbat = arcada.readBatterySensor();
+}
+
+void updateDisplay() {
+  arcada.display->fillScreen(ARCADA_BLACK);
+  arcada.display->setCursor(0, 0);
+  if (buttons & ARCADA_BUTTONMASK_A) {
+    arcada.display->drawCircle(145, 100, 10, ARCADA_RED);
+  }
+  if (buttons & ARCADA_BUTTONMASK_B) {
+    arcada.display->drawCircle(120, 100, 10, ARCADA_YELLOW);
+  }
+  if (buttons & ARCADA_BUTTONMASK_SELECT) {
+    arcada.display->drawRoundRect(60, 100, 20, 10, 5, ARCADA_DARKGREY);
+  }
+  if (buttons & ARCADA_BUTTONMASK_START) {
+    arcada.display->drawRoundRect(85, 100, 20, 10, 5, ARCADA_WHITE);
+  }
+
+  arcada.display->drawCircle(20, 100, 20, ARCADA_WHITE);
+  arcada.display->fillCircle(joystickX, joystickY, 5, ARCADA_WHITE);
 
 }
 
-
 uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return arcada.pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return arcada.pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  if (WheelPos < 85) {
+    return arcada.pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+    WheelPos -= 85;
+    return arcada.pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   } else {
-   WheelPos -= 170;
-   return arcada.pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    WheelPos -= 170;
+    return arcada.pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
